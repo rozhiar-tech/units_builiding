8f44179dbbeb3219058228f7dd1c1a0b5b52552b
Rozhiar Omer <rozyar.313175000@uhd.edu.iq>
2024-02-14 20:09:49 +0300

    added authintication

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// App.js
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Layout from './components/shared/Layout'
 import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
 import Products from './pages/Products'
+import { useEffect, useState } from 'react'
+import { auth, onAuthStateChanged } from './firebase/initFirebase' // Update the path
+import Login from './pages/Login'
 
 function App() {
+    const [user, setUser] = useState(null)
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setUser(user)
+        })
+
+        return () => unsubscribe()
+    }, [])
+
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Layout />}>
+                <Route path="/" element={user ? <Layout /> : <Navigate to="/login" />}>
                     <Route index element={<Dashboard />} />
                     <Route path="products" element={<Products />} />
                 </Route>
                 <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
         </Router>
     )
--- src/firebase/initFirebase.js
+++ src/firebase/initFirebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -16,7 +17,7 @@ const firebaseConfig = {
     measurementId: 'G-82CZR1TGL2'
 }
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
 
-export default app
+export { auth, onAuthStateChanged }
