cddf859db65ab0ed4fe63bb32dba201c0c5bcc17
Rozhiar Omer <rozyar.313175000@uhd.edu.iq>
2024-02-17 01:03:46 +0300

    .

--- src/lib/constants/index.jsx
+++ src/lib/constants/index.jsx
@@ -49,16 +49,16 @@ export const DASHBOARD_SIDEBAR_LINKS = [
 ]
 
 export const DASHBOARD_SIDEBAR_BOTTOM_LINKS = [
-    {
-        key: 'settings',
-        label: 'Settings',
-        path: '/settings',
-        icon: <HiOutlineCog />
-    },
-    {
-        key: 'support',
-        label: 'Help & Support',
-        path: '/support',
-        icon: <HiOutlineQuestionMarkCircle />
-    }
+    // {
+    //     key: 'settings',
+    //     label: 'Settings',
+    //     path: '/settings',
+    //     icon: <HiOutlineCog />
+    // },
+    // {
+    //     key: 'support',
+    //     label: 'Help & Support',
+    //     path: '/support',
+    //     icon: <HiOutlineQuestionMarkCircle />
+    // }
 ]
